07c71c2f947f7c5f08421cc9d3fd4c360614442b
Einar Pálsson <epp5@hi.is>
2020-10-26 10:21:44 +0000

    encode + decode tilbúin

--- scripts.js
+++ scripts.js
@@ -12,15 +12,10 @@ let testing = LETTERS_arr.join(''); //aftur til baka test
  */
 
 encode('EINAR', 3);
+decode('GKPDU', 3);
 
  function start() {
 }
-
-/*let answer = thiscode;
-    window.alert(
-      "Veit ekki hvaða aðgerð " + '"' + answer + '"' + " er. Reyndu aftur."
-    );
-}*/
 
 // Hér er gott að commenta út til að vinna í encode/decode föllum fyrst og síðan „viðmóti“ forrits
 
@@ -38,19 +33,6 @@ encode('EINAR', 3);
 function encode(str, n) {
   let newStr = str.split('');
   let tester = [];
-  /*let i = 0;
-  let j = 0;
-  do {
-
-    if (newStr[j] === LETTERS_arr[i]) {
-      newStr[j] = LETTERS_arr[i+n];
-    }
-    i++;
-    j++;
-    console.log(newStr)
-
-  } while (i < LETTERS.length & newStr[j] === LETTERS_arr[i] & j < newStr.length);
-  return str;*/
 
   for (let i = 0; i < newStr.length; i++) {
     for (let j = 0; j < LETTERS_arr.length; j++) {
@@ -66,16 +48,6 @@ function encode(str, n) {
 return str;
 }
 
-/*else if (newStr[j] === LETTERS_arr[i] & LETTERS_arr[i+n] === undefined) {
-  newStr[j] = LETTERS_arr[32-(31-n)];
-}*/
-
-
-
-
-
-
-
 /**
  * Afkóðar streng með því að hliðra honum um n stök.
  *
@@ -83,12 +55,28 @@ return str;
  * @param {number} n Hliðrun, heiltala á bilinu [0, lengd stafrófs]
  * @returns {string} Upprunalegi strengurinn hliðraður um n til vinstri
  */
-/*function decode(str, n) {
-  return str;
-}*/
+
+
+function decode(str, n) {
+  let newStr = str.split('');
+  let tester = [];
+
+  for (let i = 0; i < newStr.length; i++) {
+    for (let j = 0; j < LETTERS_arr.length; j++) {
+      if (newStr[i] === LETTERS_arr[j] & j-n >= 0) {
+        tester[i] = LETTERS_arr[j-n];
+      } else if (newStr[i] === LETTERS_arr[j] & j-n < 0) {
+        tester[i] = LETTERS_arr[j + (32-n)];
+      }
+    }
+  }
+  str = tester.join('');
+  console.log(str);
+return str;
+}
 
 console.assert(encode("A", 3) === "D", "kóðun á A með n=3 er D");
-//console.assert(decode("D", 3) === "A", "afkóðun á D með n=3 er A");
+console.assert(decode("D", 3) === "A", "afkóðun á D með n=3 er A");
 console.assert(
   encode("AÁBDÐEÉFGHIÍJKLMNOÓPRSTUÚVXYÝÞÆÖ", 32) ===
     "AÁBDÐEÉFGHIÍJKLMNOÓPRSTUÚVXYÝÞÆÖ",
@@ -99,7 +87,7 @@ console.assert(
     "DÐEÉFGHIÍJKLMNOÓPRSTUÚVXYÝÞÆÖAÁB",
   "kóðun á stafrófi með n=3"
 );
-/*console.assert(
+console.assert(
   decode("DÐEÉFGHIÍJKLMNOÓPRSTUÚVXYÝÞÆÖAÁB", 3) ===
     "AÁBDÐEÉFGHIÍJKLMNOÓPRSTUÚVXYÝÞÆÖ",
   "afkóðun á stafrófi með n=3"
@@ -107,4 +95,4 @@ console.assert(
 console.assert(
   decode(encode("HALLÓHEIMUR", 13), 13) === "HALLÓHEIMUR",
   "kóðun og afkóðun eru andhverf"
-);*/
+);
